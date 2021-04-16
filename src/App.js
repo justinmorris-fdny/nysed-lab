9eea7675ffaad0d4e17b0730dc1d7a7e70d589bf
yourUsernameForGithub <youremailhere>
2021-04-16 17:21:29 +0000

    finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
 // Be sure to uncomment these components and the data before you use them!
-// import SchoolCard from './components/SchoolCard.js';
-// import data from './nycSchoolEnrollmentData.js';
+import SchoolCard from './components/SchoolCard.js';
+import data from './nycSchoolEnrollmentData.js';
 import './App.css';
 
 function App() {
@@ -11,6 +11,10 @@ function App() {
       <Navbar/>
       <div className="container">
         <div className="row">
+     <SchoolCard school  = {data[0].ENTITY_NAME} year={data[0].YEAR} enrollmentdata={data[0].NUM_ELL}  numberfemale={data[0].NUM_FEMALE}  schooldata={data[0].NUM_ECDIS}/>
+     <SchoolCard school = {data[1].ENTITY_NAME}  year={data[1].YEAR} enrollmentdata={data[1].NUM_ELL}  numberfemale={data[1].NUM_FEMALE}  schooldata={data[1].NUM_ECDIS}/>
+     <SchoolCard school = {data[2].ENTITY_NAME}  year={data[2].YEAR} enrollmentdata={data[2].NUM_ELL}  numberfemale={data[2].NUM_FEMALE}  schooldata={data[2 ].NUM_ECDIS}/>
+     
 
         </div>
       </div>
--- src/components/SchoolCard.js
+++ src/components/SchoolCard.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './css/SchoolCard.css';
 
-function SchoolCard() {
+function SchoolCard(props) {
   return(
     <div className="col-4">
       <div className="card">
-        <h5 className="card-header">PASS SCHOOL NAME HERE</h5>
+        <h5 className="card-header">{props.school}</h5>
         <div className="card-body">
           <h5 className="card-title">
-            <span className="badge">2017-18</span>
-            <span className="badge">2016-17</span>
-            <span className="badge">Archive</span>
+            <span className="badge">{props.year}</span>
+            <span className="badge">{props.enrollmentdata}</span>
+            <span className="badge">{props.numberfemale}</span>
           </h5>
           <span className="badge badge-header">Student Data</span>
           <ul className="list-group">
-            <li className="list-group-item">Enrollment Data</li>
+            <li className="list-group-item">Enrollment Data </li>
             <li className="list-group-item">3-8 ELA Assessment Data</li>
             <li className="list-group-item">3-8 Math Assessment Data</li>
           </ul>
-          <span className="badge badge-header">School Data</span>
+          <span className="badge badge-header">{props.schooldata}</span>
           <ul className="list-group">
             <li className="list-group-item">School Report Card</li>
             <li className="list-group-item">Student and Educator Report</li>
@@ -26,6 +26,7 @@ function SchoolCard() {
         </div>
       </div>
     </div>
+    
   );
 }
 
